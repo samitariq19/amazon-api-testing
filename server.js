df9a36368277f15f4d104b425c4ddbe2c787e946
Sami Tariq <maddawgkilla@gmail.com>
2020-01-23 04:11:58 +0500

    added ssl and https

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const path = require('path');
 const express = require('express');
+const https = require('https');
+const fs = require('fs');
 // const dotenv = require('dotenv');
 const cors = require('cors');
 // const connectDB = require('./config/db');
@@ -33,6 +35,25 @@ app.get('/return', (req,res) => {
 // const PORT = process.env.PORT || 5000;
 const PORT = 5000;
 
-app.listen(PORT, () => {
-    console.log(`Server running on ${process.env.NODE_ENV} on port ${PORT}`);
+// app.listen(PORT, () => {
+//     console.log(`Server running on ${process.env.NODE_ENV} on port ${PORT}`);
+// });
+
+
+var key = fs.readFileSync(path.join(__dirname, 'ssl', 'server.key'));
+var cert = fs.readFileSync(path.join(__dirname, 'ssl', 'server.crt'));
+var options = {
+  key: key,
+  cert: cert
+};
+
+// app = express()
+app.get('/', (req, res) => {
+   res.send('Now using https..');
+});
+
+var server = https.createServer(options, app);
+
+server.listen(PORT, () => {
+  console.log("server starting on port : " + PORT)
 });
